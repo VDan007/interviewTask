4d609a083bd9c46be3b7a7344010da216ab98d3e
DanielVarjaskéri <varjaskerid@gmail.com>
2023-04-16 19:40:15 +0200

    style done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,48 @@ function App() {
 
 
   return (
-    <div>hi</div>
+    <div className='app__container'>
+      <h1>Online valutaváltó</h1>
+
+      <form action="submit">
+
+        <label 
+          htmlFor="amount"
+          className='input__section'
+        >Összeg
+            <input name='amount' type="number"
+            />
+        </label>
+
+        <label 
+          htmlFor="from"
+          className='input__section'
+        >Erről a valutáról
+          <select name="from">
+            <option value="">huf</option>
+            <option value="">eur</option>
+          </select>
+        </label>
+
+        <label 
+        htmlFor="to"
+        className='input__section'
+        >Erre a valutára
+          <select name="to">
+            <option value="">huf</option>
+            <option value="">eur</option>
+          </select>
+        </label>
+        
+        <div className='btnDiv'>
+          <button>Mehet</button>
+        </div>
+      </form>
+      <div className='result__section'>
+        <p>200 000HUF = </p>
+        <h3>620 EUR</h3>
+      </div>
+    </div>
   );
 }
 
